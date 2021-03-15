1722d31b57b3110ae4c3f0d7d6210be2bd2a47c4
Andrey Novichkov <andrey.novichkov@gmail.com>
2021-03-15 15:42:16 -0700

    [fix] Cleaned up code

--- scraper.go
+++ scraper.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/cdproto/runtime"
 	"github.com/chromedp/chromedp"
 	log "github.com/sirupsen/logrus"
 )
@@ -49,10 +48,11 @@ func scrapeAce() []Product{
 	storeCoordinates := []float32{37.89759212473446, -122.06905777332277}
 	closingHour := "19:00"
 
-
 	departments := []string{"tools"}
-	brands := []string{"dewalt"}
-	maxPageSize := 1000
+	brands := []string{"dewalt", "craftsman", "milwaukee"}
+
+	// TODO - Figure out best way to do this!
+	maxPageSize := 300
 
 	aceUrl := fmt.Sprintf("https://www.acehardware.com/departments/%v/%v?pageSize=%v", departments[0], brands[0], maxPageSize)
 
@@ -64,50 +64,26 @@ func scrapeAce() []Product{
 
 	// Navigate to page
 	if err := chromedp.Run(ctx, chromedp.Navigate(aceUrl)); err != nil {
-		panic("Error navigating to indeed url")
-	}
-	log.Info("Looking for Dewalt products in the tool deparment")
-
-	// Scroll to bottom
-	var res *runtime.RemoteObject
-	if err := chromedp.Run(ctx, chromedp.Evaluate(`window.scrollTo(0,document.body.scrollHeight);`, &res)); err != nil {
-		panic(err)
+		panic("Error navigating to Ace url")
 	}
 	
-	// Look for product titles
+	// Look for product data
 	var productTitles []*cdp.Node
-	if err := chromedp.Run(ctx, chromedp.Nodes(".mz-productlisting-title", &productTitles)); err != nil {
-		panic("Error getting product titles from ace store")
-	}
-
-	// Look for product prices
 	var productPrices []*cdp.Node
-	if err := chromedp.Run(ctx, chromedp.Nodes(".sales-price, .custom-price", &productPrices)); err != nil {
-		panic("Error getting product prices from ace store")
-	}
-
 	var productImages []*cdp.Node
-	if err := chromedp.Run(ctx, chromedp.Nodes("a > img", &productImages)); err != nil {
-		panic("Error getting product images from ace store")
-	}
-
 	var productItems []*cdp.Node
-	if err := chromedp.Run(ctx, chromedp.Nodes(".mz-productlisting", &productItems)); err != nil{
-		panic("error getting product items from ace store");
+	if err := chromedp.Run(ctx, chromedp.Nodes(".mz-productlisting-title", &productTitles), 
+								chromedp.Nodes(".sales-price, .custom-price", &productPrices), 
+								chromedp.Nodes("a > img", &productImages), 
+								chromedp.Nodes(".mz-productlisting", &productItems)); err != nil {
+		panic("Error getting product data from ace store")
 	}
+	fmt.Printf("Found %v products\n", len(productItems))
 
-	fmt.Printf("Found %v items\n", len(productItems))
-
+	// Create Product Objects and append to main list
 	for i := 1; i < len(productItems); i++ {
-		// productUrl := ""
-
 		if len(productItems[i].Children[2].Children[3].Children) == 0 {
-			// fmt.Println("Product does not have Children in sales price")
-			// fmt.Printf("Here is that product item: %v", productItems[i].Children[2].Children[3])
-			// fmt.Println("Skipping this product")
 			continue
-		}else {
-			// fmt.Println("Product has children in sales price")
 		}
 
 		productImage := fmt.Sprintf("https:%v", strings.TrimSpace(productItems[i].Children[1].Children[0].Children[0].AttributeValue("src")))
@@ -116,16 +92,11 @@ func scrapeAce() []Product{
 		productPrice:= strings.TrimSpace(productItems[i].Children[2].Children[3].Children[1].Children[0].NodeValue)
 		
 
-
-		// productTitle := productTitles[i].Children[0].NodeValue
-		// productLink := productTitles[i].AttributeValue("href")
-		// productPrice := productTitles[i]
-
-		// fmt.Printf("Found product title #%v: %v\n", i, productName)
-		// fmt.Printf("Found product image: #%v, %v\n", i, productImage)
-		// fmt.Printf("Found product url #%v: %v\n", i, productURL)
-		// fmt.Printf("Found product price #%v: %v\n", i, productPrice)
-		// fmt.Println("------")
+		fmt.Printf("Found product title #%v: %v\n", i, productName)
+		fmt.Printf("Found product image: #%v, %v\n", i, productImage)
+		fmt.Printf("Found product url #%v: %v\n", i, productURL)
+		fmt.Printf("Found product price #%v: %v\n", i, productPrice)
+		fmt.Println("---------------------------------------------")
 
 		newProduct := Product{
 			StoreName: storeName,
@@ -149,3 +120,10 @@ func scrapeAce() []Product{
 func scrapeCvs() {
 	log.Info("Starting to scrape CVS")
 }
+
+
+// // Scroll to bottom
+	// var res *runtime.RemoteObject
+	// if err := chromedp.Run(ctx, chromedp.Evaluate(`window.scrollTo(0,document.body.scrollHeight);`, &res)); err != nil {
+	// 	panic(err)
+	// }
