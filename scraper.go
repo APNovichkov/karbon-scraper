c4bbc1423cb1ba4ab18079460e31b6a11a880b00
Andrey Novichkov <andrey.novichkov@gmail.com>
2021-03-15 16:47:14 -0700

    [add] Added concurrency to app

--- scraper.go
+++ scraper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
@@ -27,17 +28,40 @@ type Product struct {
 func RunScraper() []Product{
 	log.Info("Running Scraper Module")
 
-	var allProducts []Product
+	wg := sync.WaitGroup{}
+	allProducts := []Product{}
+	productsChan := make(chan Product, 1000000)
 
-	aceProducts := scrapeAce();
+	// Run Ace Scraper
+	wg.Add(1)
+	go scrapeAce(&wg, productsChan);
 
-	allProducts = append(allProducts, aceProducts...)
+	// Defer closing of channel until waitgroup is clear
+	go func() {
+		defer close(productsChan)
+        wg.Wait()
+	}()
+
+	// Read in items from channel
+	for i := 0; i < 100000; i++ {
+        select {
+        case product, ok := <- productsChan:
+            if !ok {
+                productsChan = nil
+            }
+            allProducts = append(allProducts, product)
+		}
+        if productsChan == nil {
+			break
+        }
+    }
 
 	return allProducts
 }
 
 
-func scrapeAce() []Product{
+func scrapeAce(wg *sync.WaitGroup, productsChan chan Product) []Product{
+	defer wg.Done()
 
 	aceProducts := []Product{}
 
@@ -48,70 +72,74 @@ func scrapeAce() []Product{
 	storeCoordinates := []float32{37.89759212473446, -122.06905777332277}
 	closingHour := "19:00"
 
-	departments := []string{"tools"}
-	brands := []string{"dewalt", "craftsman", "milwaukee"}
-
-	// TODO - Figure out best way to do this!
-	maxPageSize := 300
-
-	aceUrl := fmt.Sprintf("https://www.acehardware.com/departments/%v/%v?pageSize=%v", departments[0], brands[0], maxPageSize)
-
-	// create context
-	log.Info("Initializing Context for Ace Scraper")
-	ctx, cancel := chromedp.NewContext(context.Background())
-	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
-	defer cancel()
-
-	// Navigate to page
-	if err := chromedp.Run(ctx, chromedp.Navigate(aceUrl)); err != nil {
-		panic("Error navigating to Ace url")
+	dataPoints := map[string][]string{
+		"tools": {"dewalt", "craftsman", "milwaukee"},
 	}
-	
-	// Look for product data
-	var productTitles []*cdp.Node
-	var productPrices []*cdp.Node
-	var productImages []*cdp.Node
-	var productItems []*cdp.Node
-	if err := chromedp.Run(ctx, chromedp.Nodes(".mz-productlisting-title", &productTitles), 
-								chromedp.Nodes(".sales-price, .custom-price", &productPrices), 
-								chromedp.Nodes("a > img", &productImages), 
-								chromedp.Nodes(".mz-productlisting", &productItems)); err != nil {
-		panic("Error getting product data from ace store")
-	}
-	fmt.Printf("Found %v products\n", len(productItems))
 
-	// Create Product Objects and append to main list
-	for i := 1; i < len(productItems); i++ {
-		if len(productItems[i].Children[2].Children[3].Children) == 0 {
-			continue
-		}
-
-		productImage := fmt.Sprintf("https:%v", strings.TrimSpace(productItems[i].Children[1].Children[0].Children[0].AttributeValue("src")))
-		productURL := strings.TrimSpace(productItems[i].Children[2].Children[0].AttributeValue("href")) 
-		productName := strings.TrimSpace(productItems[i].Children[2].Children[0].Children[0].NodeValue)
-		productPrice:= strings.TrimSpace(productItems[i].Children[2].Children[3].Children[1].Children[0].NodeValue)
+	for _, searchKey := range(dataPoints["tools"]) {
 		
+		// TODO - Figure out best way to do this!
+		maxPageSize := 300
 
-		fmt.Printf("Found product title #%v: %v\n", i, productName)
-		fmt.Printf("Found product image: #%v, %v\n", i, productImage)
-		fmt.Printf("Found product url #%v: %v\n", i, productURL)
-		fmt.Printf("Found product price #%v: %v\n", i, productPrice)
-		fmt.Println("---------------------------------------------")
+		aceUrl := fmt.Sprintf("https://www.acehardware.com/departments/%v/%v?pageSize=%v", "tools", searchKey, maxPageSize)
 
-		newProduct := Product{
-			StoreName: storeName,
-			ClosingHour: closingHour,
-			PhoneNumber: phoneNumber,
-			Address: address,
-			StoreCoordinates: storeCoordinates,
-			ProductName: productName,
-			ProductURL: productURL,
-			ProductImageUrl: productImage,
-			InStorePrice: productPrice,
-			OriginalPrice: productPrice,
+		// create context
+		log.Info("Initializing Context for Ace Scraper")
+		ctx, cancel := chromedp.NewContext(context.Background())
+		ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+		defer cancel()
+
+		// Navigate to page
+		if err := chromedp.Run(ctx, chromedp.Navigate(aceUrl)); err != nil {
+			panic("Error navigating to Ace url")
 		}
 
-		aceProducts = append(aceProducts, newProduct)
+		// Look for product data
+		var productTitles []*cdp.Node
+		var productPrices []*cdp.Node
+		var productImages []*cdp.Node
+		var productItems []*cdp.Node
+		if err := chromedp.Run(ctx, chromedp.Nodes(".mz-productlisting-title", &productTitles), 
+									chromedp.Nodes(".sales-price, .custom-price", &productPrices), 
+									chromedp.Nodes("a > img", &productImages), 
+									chromedp.Nodes(".mz-productlisting", &productItems)); err != nil {
+			panic("Error getting product data from ace store")
+		}
+		fmt.Printf("Found %v products\n", len(productItems))
+
+		// Create Product Objects and append to main list
+		for i := 1; i < len(productItems); i++ {
+			if len(productItems[i].Children[2].Children[3].Children) == 0 {
+				continue
+			}
+
+			productImage := fmt.Sprintf("https:%v", strings.TrimSpace(productItems[i].Children[1].Children[0].Children[0].AttributeValue("src")))
+			productURL := strings.TrimSpace(productItems[i].Children[2].Children[0].AttributeValue("href")) 
+			productName := strings.TrimSpace(productItems[i].Children[2].Children[0].Children[0].NodeValue)
+			productPrice:= strings.TrimSpace(productItems[i].Children[2].Children[3].Children[1].Children[0].NodeValue)
+			
+
+			fmt.Printf("Found product title #%v: %v\n", i, productName)
+			fmt.Printf("Found product image: #%v, %v\n", i, productImage)
+			fmt.Printf("Found product url #%v: %v\n", i, productURL)
+			fmt.Printf("Found product price #%v: %v\n", i, productPrice)
+			fmt.Println("---------------------------------------------")
+
+			newProduct := Product{
+				StoreName: storeName,
+				ClosingHour: closingHour,
+				PhoneNumber: phoneNumber,
+				Address: address,
+				StoreCoordinates: storeCoordinates,
+				ProductName: productName,
+				ProductURL: productURL,
+				ProductImageUrl: productImage,
+				InStorePrice: productPrice,
+				OriginalPrice: productPrice,
+			}
+
+			productsChan <- newProduct
+		}
 	}
 
 	return aceProducts
